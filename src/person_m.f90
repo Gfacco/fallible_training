6450ae8200c0172b06af4d6380b28409d68540d8
gfacco <gfaccou@archaeologic.codes>
2022-12-12 09:13:25 -0800

    refac: minor edits for better tests

--- src/person_m.f90
+++ src/person_m.f90
@@ -4,6 +4,7 @@ implicit none
 
 public :: person_t
 type :: person_t
+  private
   type(varying_string) :: name_
 contains
   procedure :: name
@@ -15,12 +16,15 @@ end interface
 
 contains
   pure function construct(name) result(person)
+    implicit none
+
     type(person_t) :: person
     type(varying_string), intent(in) :: name
     person%name_ = name
   end function
 
   pure function name(self)
+    implicit none
     class(person_t), intent(in) :: self
     type(varying_string) :: name
     name = self%name_
